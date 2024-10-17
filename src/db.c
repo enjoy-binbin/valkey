12f930a3515dbb73f0179657bb42210439f6bb56
Binbin <binloveplay1314@qq.com>
2024-10-17 16:51:05 +0800

    Disable expire during slot migration
    Let's take expire as an example. If the target node
    deleted an expired key or the key is logic expires
    during the process of the loading of the slot RDB,
    the source node may propagate the expire command after
    loading the slot RDB. These expire commands will become
    invalid on the logically expired keys, resulting in
    data loss.
    
    During the slot migration process, the keys are not
    considered expired in the expiration check of the
    command propagated by the source node.

--- src/db.c
+++ src/db.c
@@ -1834,8 +1834,11 @@ keyStatus expireIfNeededWithDictIndex(serverDb *db, robj *key, int flags, int di
      *
      * When replicating commands from the primary, keys are never considered
      * expired. */
-    if (server.primary_host != NULL) {
+    if (server.primary_host != NULL || isSlotSyncInProgress()) {
         if (server.current_client && (server.current_client->flag.primary)) return KEY_VALID;
+        /* When replicating commands from the slot sync source node,
+         * keys are never considered expired. */
+        if (server.current_client->slotsync_link) return 0;
         if (!(flags & EXPIRE_FORCE_DELETE_EXPIRED)) return KEY_EXPIRED;
     } else if (server.import_mode) {
         /* If we are running in the import mode on a primary, instead of
